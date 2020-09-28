ed612a5b482bc59adf4790d85f63e24ae811e633
CHUNGGAK LEE <chgale@outlook.kr>
2020-09-28 17:17:54 +0900

    Add alzheimer/r/path_analysis.R

---
+++ alzheimer/r/path_analysis.R
@@ -0,0 +1,70 @@
+library(lavaan)
+library(tidyverse)
+library(semPlot)
+
+setwd("C:/Users/Administrator/wd/alzheimer/path analysis")
+
+ori_work_data <- data.frame()
+for (filename in list.files("data")) {
+    ori_work_data <- bind_rows(ori_work_data,
+                               read_csv(file.path("data", filename),
+                                        col_names = FALSE,
+                                        col_types = cols(.default = "c"),
+                                        skip = 1))
+}
+
+work_data <- ori_work_data %>% 
+    select(-1) %>% 
+    setNames(., c("id", "registration_year", "age", "gender", "weight",
+                  "height", "bmi", "smoking_amount", "smoking_period",
+                  "smoking_total", "alcohol_consumption", "drinking_frequency",
+                  "drinking_total", "num_of_workouts_per_week", "htn",
+                  "diabetes", "cardiac_disease", "dyslipidemia",
+                  "breast_ca_icd", "breast_ca_ctx", "breast_ca_ht",
+                  "breast_ca_date", "death_date", "death_age", "brain_meta",
+                  "dementia_code", "dementia_date", "donepezil", "rivastigmine",
+                  "galatamine", "memntine", "time")) %>% 
+    mutate(age = as.integer(age),
+           bmi = as.double(bmi),
+           smoking_total = as.double(smoking_total),
+           drinking_total = as.double(drinking_total),
+           num_of_workouts_per_week = as.double(num_of_workouts_per_week),
+           htn = recode(htn, `0` = FALSE, `1` = TRUE),
+           diabetes = recode(diabetes, `0` = FALSE, `1` = TRUE),
+           cardiac_disease = recode(cardiac_disease, `0` = FALSE, `1` = TRUE),
+           dyslipidemia = recode(dyslipidemia, `0` = FALSE, `1` = TRUE),
+           breast_ca = ifelse(breast_ca_icd == "#N/A", FALSE, TRUE),
+           breast_ca_ctx = ifelse(breast_ca_ctx %in% c("#N/A", 0), FALSE, TRUE),
+           breast_ca_ht = ifelse(breast_ca_ctx %in% c("#N/A", 0), FALSE, TRUE),
+           dementia = ifelse(dementia_code == "#N/A", FALSE, TRUE))
+
+model <- "
+breast_ca ~ age + htn + diabetes + cardiac_disease + dyslipidemia
+dementia ~ breast_ca + breast_ca_ctx + age + bmi + smoking_total + drinking_total
+"
+
+result <- sem(model = model,
+              data = work_data)
+
+semPaths(result, "std", style = "lisrel")
+
+obj <- semPlotModel(result)
+ori_pars <- obj@Pars
+check_pars <- obj@Pars %>% 
+    filter(!(edge %in% c("int", "<->") | lhs == rhs))
+keep_pars <- obj@Pars %>% 
+    filter(edge %in% c("int", "<->") | lhs == rhs)
+test_against <- standardizedSolution(result) %>% 
+    filter(pvalue < 0.05 & rhs != lhs)
+test_against_rev <- test_against %>% 
+    rename(tmp = lhs, lhs = rhs) %>% 
+    rename(rhs = tmp)
+checked_pars <- check_pars %>% 
+    semi_join(test_against, by = c("lhs", "rhs")) %>% 
+    bind_rows(check_pars %>% 
+                  semi_join(test_against_rev, by = c("lhs", "rhs")))
+obj@Pars <- keep_pars %>% 
+    bind_rows(checked_pars)
+anti_join(ori_pars, obj@Pars)
+
+semPaths(obj, "std")
