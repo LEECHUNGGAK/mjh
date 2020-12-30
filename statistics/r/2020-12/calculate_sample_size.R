7936e7f5f3745ab5c86e0b49125b99a62250f976
CHUNGGAK LEE <chgale@outlook.kr>
2020-12-30 17:19:01 +0900

    Add statstics/r/2020-12/calculate_sample_size.R

---
+++ statistics/r/2020-12/calculate_sample_size.R
@@ -0,0 +1,30 @@
+install.packages(c("pwr", "WebPower"))
+
+library(pwr)
+library(WebPower)
+
+calculate_d <- function(m1, m2, s1, s2) {
+    d <- (m2 - m1) / sqrt((s1^2 + s2^2) / 2)
+    return(d)
+}
+
+# One Mean T-test
+pwr.t.test(d = 0.8,
+           sig.level = 0.05,
+           power = 0.8,
+           type = "one.sample",
+           alternative = "two.sided")
+
+# Two Means T-test
+pwr.t.test(d = calculate_d(m1 = 8.08, m2 = 7.15, s1 = 2.8, s2 = 1.91),
+           sig.level = 0.05,
+           power = 0.8,
+           type = "two.sample",
+           alternative = "two.sided")
+
+# Paired T-test
+pwr.t.test(d = calculate_d(m1 = 8.08, m2 = 7.49, s1 = 2.8, s2 = 2.57),
+           sig.level = 0.05,
+           power = 0.8,
+           type = "paired",
+           alternative = "two.sided")
