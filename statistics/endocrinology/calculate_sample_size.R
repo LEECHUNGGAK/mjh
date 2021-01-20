bb6e64b8374679f7deff2d5d44b7db70165180cf
CHUNGGAK LEE <chgale@outlook.kr>
2021-01-20 10:08:33 +0900

    Organize outline

--- statistics/endocrinology/calculate_sample_size.R
+++ statistics/endocrinology/calculate_sample_size.R
@@ -8,21 +8,32 @@ calculate_d <- function(m1, m2, s1, s2) {
     return(d)
 }
 
-# One Mean T-test
+
+# One Mean T-test ---------------------------------------------------------
 pwr.t.test(d = 0.7 / 0.47,
            sig.level = 0.05,
            power = 0.8,
            type = "one.sample",
            alternative = "two.sided")
 
-# Two Means T-test
+
+# Two Means T-test --------------------------------------------------------
+# Use difference and standard deviation
+pwr.t.test(d = 1/1.3,
+           sig.level = 0.05,
+           power = 0.8,
+           type = "two.sample",
+           alternative = "two.sided")
+
+# Use calculated_d function
 pwr.t.test(d = calculate_d(m1 = 8.08, m2 = 7.15, s1 = 2.8, s2 = 1.91),
            sig.level = 0.05,
            power = 0.8,
            type = "two.sample",
            alternative = "two.sided")
 
-# Paired T-test
+
+# Paired T-test -----------------------------------------------------------
 pwr.t.test(d = calculate_d(m1 = 8.08, m2 = 7.49, s1 = 2.8, s2 = 2.57),
            sig.level = 0.05,
            power = 0.8,
