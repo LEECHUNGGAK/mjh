ed7861afc7e807f0af97b127d574cdf5eec79877
CHUNGGAK LEE <chgale@outlook.kr>
2021-03-11 12:31:29 +0900

    Update

--- statistics/laboratory_medicine/analyze.R
+++ statistics/laboratory_medicine/analyze.R
@@ -14,6 +14,31 @@ process_data <- function(x, method, index) {
                relocate(method, .before = method_date))
 }
 
+analyze <- function(dat, target, comparator) {
+    x <- dat %>% 
+        filter(method == target) %>% 
+        select(id, method, num, result) %>% 
+        full_join(dat %>% 
+                      filter(method == comparator) %>% 
+                      select(id, method, num, result),
+                  by = c("id", "num")) %>% 
+        select(result.x, result.y) %>% 
+        rename(target = 1, comparator = 2)
+    
+    # x <- data.frame(target = dat %>% 
+    #                     filter(method == target) %>% 
+    #                     pull(result),
+    #                 comparator = dat %>% 
+    #                     filter(method == comparator) %>% 
+    #                     pull(result))
+    
+    cat(paste0("Target: ", target, "\nComparator: ", comparator, "\n"))
+    print(table(x))
+    print(agree(x))
+    print(kappa2(x))
+    cat("\n")
+}
+
 # Process data ------------------------------------------------------------
 r <- read_csv("C:/Users/Administrator/work/1_statistics/1_laboratory_medicine/data/data.csv",
               col_names = FALSE,
@@ -35,30 +60,39 @@ d <- d %>%
     mutate(condition_date = ymd(condition_date),
            onset_date = ymd(onset_date),
            method_date = ymd(method_date),
-           condition_date_diff = method_date - condition_date,
-           onset_date_diff = method_date - onset_date)
+           condition_date_diff = as.integer(method_date - condition_date),
+           onset_date_diff = as.integer(method_date - onset_date),
+           condition_date_diff_group = cut(condition_date_diff,
+                                           breaks = c(1, 7, 14, 21, 28, Inf),
+                                           include.lowest = TRUE),
+           onset_date_diff_group = cut(onset_date_diff,
+                                       breaks = c(1, 7, 14, 21, 28, Inf),
+                                       include.lowest = TRUE)) %>% 
+    group_by(id, method) %>% 
+    mutate(num = row_number()) %>% 
+    ungroup()
 
 # Analyze -----------------------------------------------------------------
 # Reproducibility
-s_combn <- combn(s$method, 2)
+s_combn <- matrix(c("saliva", "naso", "saliva", "sputum"), ncol = 2)
+
 sink("C:/Users/Administrator/work/1_statistics/1_laboratory_medicine/output.txt")
+
 for (i in 1:ncol(s_combn)) {
     var_t <- s_combn[1, i]
     var_c <- s_combn[2, i]
     
-    t <- data.frame(target = d %>% 
-                            filter(method == var_t) %>% 
-                            pull(result),
-                        comparator = d %>% 
-                            filter(method == var_c) %>% 
-                            pull(result))
+    analyze(d, var_t, var_c)
     
-    cat(paste0("Target: ", var_t, "\nComparator: ", var_c, "\n"))
-    print(table(t))
-    print(agree(t))
-    print(kappa2(t))
-    cat("\n")
+    for (j in levels(d$condition_date_diff_group)) {
+        for (k in c("condition", "onset")) {
+            cat(paste0("Filter: ", k, "_date_diff_group == ", j, "\n"))
+            filter_d <- d %>% 
+                filter(!!sym(paste0(k, "_date_diff_group")) == j)
+            
+            analyze(filter_d, var_t, var_c)
+        }
+    }
 }
-sink()
 
-# 
+sink()
