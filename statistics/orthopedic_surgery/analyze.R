a0900490a35c8982e01f5042e80bc6b23ed6647d
CHUNGGAK LEE <chgale@outlook.kr>
2021-03-24 17:29:38 +0900

    Update

--- statistics/orthopedic_surgery/analyze.R
+++ statistics/orthopedic_surgery/analyze.R
@@ -2,15 +2,25 @@
 library(tidyverse)
 library(readxl)
 
-d_raw <- read_excel("C:/Users/Administrator/work/1_statistics/orthopedic_surgery/rTSA total list before looking at SBS 최종.xlsx",
-                    col_types = "text",
-                    skip = 1) %>% 
-    rename(post_er_at_side = 26, post_er_90 = 27, post_ir_90 = 28,
-           post_ir_back = 29, post_er_strength = 30, post_ir_strength = 31,
-           sbs = 38)
-
 v <- c("post_er_at_side", "post_er_90", "post_ir_90", "post_ir_back",
        "post_er_strength", "post_ir_strength")
+
+d_raw <- read_excel("C:/Users/Administrator/work/1_통계자문/orthopedic_surgery/OS 이용걸교수님 논문통계데이터.xlsx",
+                    col_types = "text",
+                    skip = 1) %>% 
+    rename(post_er_at_side = 27, post_er_90 = 28, post_ir_90 = 29,
+           post_ir_back = 31, post_er_strength = 32, post_ir_strength = 33,
+           sbs = 40) %>% 
+    select(sbs, all_of(v)) %>% 
+    mutate(sbs = as.factor(as.integer(sbs)),
+           post_er_at_side = as.integer(post_er_at_side),
+           post_er_90 = as.integer(post_er_90),
+           post_ir_90 = as.integer(post_ir_90),
+           post_ir_back = as.integer(post_ir_back),
+           post_er_strength = as.double(post_er_strength),
+           post_ir_strength = as.double(post_ir_strength)) %>% 
+    drop_na(sbs)
+
 
 # Analyze -----------------------------------------------------------------
 for (i in v) {
